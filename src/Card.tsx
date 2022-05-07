facc2f42b6d7eae2a868cff5306e42917153fb97
JunIce <east.qrj@gmail.com>
2022-05-07 20:11:03 +0800

    feat: request server data by debounce

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,12 +1,15 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useCallback, useEffect, useState } from "react";
 import "./Card.css";
-import { classnames } from "./utils/util";
+import { AxiosInstance } from "./utils/request";
+import { classnames, debounce } from "./utils/util";
 
 const QQREG = /\d{5,11}/;
 
 export const Card: React.FC<{}> = () => {
+  const [value, setValue] = useState<string>("");
   const [valid, setValid] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
+  const [resp, setResp] = useState<ServerResponseData | null>(null);
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     let value = e.target.value + "";
@@ -19,24 +22,59 @@ export const Card: React.FC<{}> = () => {
     // set value to origin input element
     e.target.value = value;
 
-    setValid(!QQREG.test(value));
+    let isValid = QQREG.test(value);
+
+    setValid(!isValid);
+    // only fetch data when value is correct qq number
+    isValid && setValue(value);
   };
 
+
+
+  useEffect(() => {
+    fetchData({ qq: value });
+  }, [value]);
+
+  const fetchServerData = (params: any) => {
+    setLoading(true);
+    AxiosInstance.get("https://api.uomg.com/api/qq.info", params)
+      .then((res: ServerResponseData) => setResp(res))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const fetchData = useCallback(debounce(fetchServerData, 800), []);
+
   const renderResult = () => {
-    return loading ? (
-      <div className="content-wrapper">
-        <img className="icon-loading" src="/assets/loading.svg" alt="loading" />
-      </div>
-    ) : (
+    if (loading) {
+      return (
+        <div className="content-wrapper">
+          <img
+            className="icon-loading"
+            src="/assets/loading.svg"
+            alt="loading"
+          />
+        </div>
+      );
+    }
+
+    if (!resp) return;
+
+    if (resp?.code !== 1) {
+      return (
+        <div className="error-tip">
+          <div className="error-tip_text">{resp?.msg || "服务器异常"}</div>
+        </div>
+      );
+    }
+
+    return (
       <div className="card-item">
-        <img
-          className="card-item_avatar"
-          src="https://q2.qlogo.cn/headimg_dl?spec=100&dst_uin=791773678"
-          alt=""
-        />
+        <img className="card-item_avatar" src={resp.qlogo} alt={resp.name} />
         <div className="card-item_info">
-          <div className="card-item_username">F@FO</div>
-          <div className="card-item_number">791773678</div>
+          <div className="card-item_username">{resp.name}</div>
+          <div className="card-item_number">{resp.qq}</div>
         </div>
       </div>
     );
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,14 @@
 type ClassListData = {
   [key: string]: boolean;
 };
+
+
+
+type ServerResponseData = {
+  code: number;
+  msg?: string;
+
+  name?: string;
+  qlogo?: string;
+  qq?: string;
+}
